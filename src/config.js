f2b3a80231c2cd16d67d6d18eabbff6a090c9eda
cmsnow <cyuan123@live.com>
2018-01-22 13:51:15 -0800

    updatre

--- src/config.js
+++ src/config.js
@@ -43,7 +43,7 @@ var defaults = module.exports = {
         invalidExpireDate: 'The expiration date appears to be invalid.',
         paymentProcessing: 'Payment processing. Please wait!',
         pleaseEnter: 'Please enter ',
-	doneMsg: 'Finished/Click Me',
+	doneMsg: 'Completed/Click to Close',
         empty: 'Your shopping cart is empty'
     }
 
--- src/util/currency.js
+++ src/util/currency.js
@@ -76,7 +76,7 @@ module.exports = function currency(amount, config) {
         result = amount;
 
     if (config && config.format) {
-        result = before + result.toFixed(length) + after;
+        result = before + ' ' + result.toFixed(length) + after;
     }
 
     if (showCode) {
